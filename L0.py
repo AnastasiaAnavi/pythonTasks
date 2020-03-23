3d086c28deb497f2dd25a7216925b0f870e82a6e
AnastasiaAnavi <46967706+AnastasiaAnavi@users.noreply.github.com>
2020-03-23 13:48:56 +0200

    Add files via upload

---
+++ L0.py
@@ -0,0 +1,21 @@
+# :D
+print ('Як тебе звати?')
+name=input()
+print ('Дороу, ' +name)
+print ('Скільки тобі років?')
+age=input()
+print ('Клас! ' +age, 'найкращий вік!')
+print ('Де ти живеш?')
+city=input()
+print ('Вау, ' +city, 'то дуже гарне місто!')
+
+#згідно умов домашки
+print ('What is your name?')
+name=input()
+print ('How old are you?')
+age=input()
+print ('Where do you live?')
+city=input()
+print ('Hello, '+name)
+print ('Your age is '+age)
+print ('You live in '+city)
